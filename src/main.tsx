31efba17ef2d391817b907bba8be24729f179340
Mikel Edwin <michealanthonyxxx@gmail.com>
2024-02-05 23:01:33 +0100

    Added Dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { HelmetProvider } from 'react-helmet-async'
+import { Outlet, RouterProvider, ScrollRestoration, createBrowserRouter } from 'react-router-dom'
+
+const Layout = () => (
+  <>
+    <ScrollRestoration/>
+    <Outlet/>
+  </>
+)
+
+const Routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        index: true,
+        element: <App/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <RouterProvider router={Routes}/>
+    </HelmetProvider>
   </React.StrictMode>,
 )
